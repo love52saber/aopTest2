30f2b64aa3ee3532f8e610d71bd5646aa742d9ad
love52saber <1457659650@qq.com>
2021-03-11 23:01:57 +0800

    1

---
+++ src/main/java/com/youngfeng/designmode/proxy2/Object.java
@@ -0,0 +1,8 @@
+package com.youngfeng.designmode.proxy2;
+
+public class Object {
+
+    public static void main(String[] args) {
+        System.out.println("wsnd");
+    }
+}
---
+++ src/main/java/java/lang/Object.java
@@ -0,0 +1,8 @@
+package java.lang;
+
+public class Object {
+
+    public static void main(String[] args) {
+        System.out.println("wsnd");
+    }
+}
